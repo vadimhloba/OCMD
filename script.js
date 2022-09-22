6c5d56dbc542d31151fe58d1bf77cacbe8e974cb
Yosuphew <vadimirua1409@gmail.com>
2022-09-22 21:27:14 +0300

    added some changes

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const menuButton = document.querySelector('.menu-nav')
 const headerNav = document.querySelector('.header-main nav')
+const menuClose = document.querySelector('.nav-active')
 menuButton.addEventListener('click', () => {
 	headerNav.classList.add('active')
+	menuClose.classList.add('active')
+})
+menuClose.addEventListener('click', () => {
+	headerNav.classList.add('inactive')
+	menuClose.classList.add('inactive')
 })

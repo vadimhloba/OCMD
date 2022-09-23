275d45af4f03bde0592465e0a7f6736891e1a927
Yosuphew <vadimirua1409@gmail.com>
2022-09-23 15:08:03 +0300

    added some changes

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const menuButton = document.querySelector('.menu-nav')
 const headerNav = document.querySelector('.header-main nav')
 const buttonActive = document.querySelector('.header-main .button-active')
+const imgAnim = document.querySelector('.header-main nav.active img')
 menuButton.addEventListener('click', () => {
 	headerNav.classList.toggle('active')
 	buttonActive.classList.toggle('active')
+	imgAnim.classList.toggle('active')
 })

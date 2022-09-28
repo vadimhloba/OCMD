b6ffb880c7ed555549dcef8f1973bd2b06459daa
Yosuphew <vadimirua1409@gmail.com>
2022-09-28 19:47:25 +0300

    added some changes

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+/*----------START menu button----------*/
 const menuButton = document.querySelector('.menu-nav')
 const headerNav = document.querySelector('.header-main nav')
 menuButton.addEventListener('click', e => {
 	headerNav.classList.toggle('active')
 	e.target.classList.toggle('active')
 })
-
+/*----------END menu button----------*/
+/*----------START save-product----------*/
 const clickIcon = document.querySelector('.product .save-product .icons button')
 const productDiv = document.querySelector('.product .save-product')
 clickIcon.addEventListener('click', () => {
 	productDiv.classList.toggle('active')
 })
+/*----------END save-product----------*/
+/*----------START slider----------*/
+const sliderButton = document.querySelector('')
+/*----------END slider----------*/
